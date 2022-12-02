1b5ba40c1cbae5d287f0fbc94041d3c4b7870505
sojungpp <sophie2458@naver.com>
2022-12-02 14:23:42 +0900

    first setting

---
+++ EB_Java/src/Framework/EventId.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright(c) 2021 All rights reserved by Jungho Kim in MyungJi University 
+ */
+
+package Framework;
+
+// 이벤트 넣고 싶으면 여기에 추가
+public enum EventId {
+    ClientOutput, ListStudents, ListCourses, ListStudentsRegistered, ListCourseRegistered, ListCoursesCompleted, RegisterStudents, DeleteStudents, RegisterCourses, DeleteCourses, QuitTheSystem
+}
